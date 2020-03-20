783557d4c19309ac16779e243215af0d688ab248
James Page <james.page@ubuntu.com>
2020-03-20 10:43:36 +0000

    General tidy

--- mlnx_switchdev_mode/sriovify.py
+++ mlnx_switchdev_mode/sriovify.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
+#
+# Copyright 2019 Canonical Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import argparse
 import json
@@ -8,32 +22,31 @@ import subprocess
 
 
 class PCIDevice(object):
-
     def __init__(self, pci_addr: str):
         self.pci_addr = pci_addr
 
     @property
     def path(self) -> str:
-        return '/sys/bus/pci/devices/{}'.format(self.pci_addr)
+        return "/sys/bus/pci/devices/{}".format(self.pci_addr)
 
     def subpath(self, subpath: str) -> str:
-        return '{}/{}'.format(self.path, subpath)
+        return "{}/{}".format(self.path, subpath)
 
     @property
     def driver(self) -> str:
-        return os.path.basename(os.readlink(self.subpath('driver')))
+        return os.path.basename(os.readlink(self.subpath("driver")))
 
     @property
     def bound(self) -> bool:
-        return os.path.exists(self.subpath('driver'))
+        return os.path.exists(self.subpath("driver"))
 
     @property
     def is_pf(self) -> bool:
-        return os.path.exists(self.subpath('sriov_numvfs'))
+        return os.path.exists(self.subpath("sriov_numvfs"))
 
     @property
     def is_vf(self) -> bool:
-        return os.path.exists(self.subpath('physfn'))
+        return os.path.exists(self.subpath("physfn"))
 
     @property
     def vf_addrs(self) -> list:
@@ -43,9 +56,7 @@ class PCIDevice(object):
             try:
                 vf_addrs.append(
                     os.path.basename(
-                        os.readlink(
-                            self.subpath('virtfn{}'.format(i))
-                        )
+                        os.readlink(self.subpath("virtfn{}".format(i)))
                     )
                 )
             except FileNotFoundError:
@@ -59,17 +70,28 @@ class PCIDevice(object):
 
     def devlink_get(self, obj_name: str):
         out = subprocess.check_output(
-            ['/sbin/devlink', 'dev', obj_name,
-             'show', 'pci/{}'.format(self.pci_addr),
-             '--json']
+            [
+                "/sbin/devlink",
+                "dev",
+                obj_name,
+                "show",
+                "pci/{}".format(self.pci_addr),
+                "--json",
+            ]
         )
-        return json.loads(out)['dev']['pci/{}'.format(self.pci_addr)]
+        return json.loads(out)["dev"]["pci/{}".format(self.pci_addr)]
 
     def devlink_set(self, obj_name: str, prop: str, value: str):
         subprocess.check_call(
-            ['/sbin/devlink', 'dev', obj_name,
-             'set', 'pci/{}'.format(self.pci_addr),
-             prop, value]
+            [
+                "/sbin/devlink",
+                "dev",
+                obj_name,
+                "set",
+                "pci/{}".format(self.pci_addr),
+                prop,
+                value,
+            ]
         )
 
     def __str__(self) -> str:
@@ -77,15 +99,15 @@ class PCIDevice(object):
 
 
 def netdev_sys(netdev: str, path: str) -> str:
-    return os.path.join('/sys/class/net', netdev, path)
+    return os.path.join("/sys/class/net", netdev, path)
 
 
 def build_pci_to_netdev() -> dict:
     pci_to_netdev = {}
-    for netdev in os.listdir('/sys/class/net'):
+    for netdev in os.listdir("/sys/class/net"):
         try:
             pcidev = os.path.basename(
-                os.readlink(netdev_sys(netdev, 'device'))
+                os.readlink(netdev_sys(netdev, "device"))
             )
         except (FileNotFoundError, NotADirectoryError):
             continue
@@ -95,68 +117,73 @@ def build_pci_to_netdev() -> dict:
 
 def netdev_is_pf(netdev: str) -> bool:
     try:
-        return os.path.exists(netdev_sys(netdev, 'device/sriov_numvfs'))
+        return os.path.exists(netdev_sys(netdev, "device/sriov_numvfs"))
     except (FileNotFoundError, NotADirectoryError):
         return False
 
 
 def netdev_is_vf(netdev: str) -> bool:
     try:
-        return os.path.exists(netdev_sys(netdev, 'device/physfn'))
+        return os.path.exists(netdev_sys(netdev, "device/physfn"))
     except (FileNotFoundError, NotADirectoryError):
         return False
 
 
 def netdev_get_pf_pci(netdev: str) -> str:
     assert netdev_is_vf(netdev)
-    return os.path.basename(os.readlink(netdev_sys(netdev, 'device/physfn')))
+    return os.path.basename(os.readlink(netdev_sys(netdev, "device/physfn")))
 
 
 def netdev_get_driver(netdev: str) -> str:
-    return os.path.basename(os.readlink(netdev_sys(netdev, 'device/driver')))
+    return os.path.basename(os.readlink(netdev_sys(netdev, "device/driver")))
 
 
 def show():
     """Show details of all installed network adapters"""
     pci_to_netdev = build_pci_to_netdev()
     for pci, netdev in sorted(pci_to_netdev.items()):
-        suffix = ''
+        suffix = ""
         if netdev_is_pf(netdev):
-            suffix = 'PF'
+            suffix = "PF"
         elif netdev_is_vf(netdev):
             phys_netdev = pci_to_netdev[netdev_get_pf_pci(netdev)]
-            suffix = 'VF of %s' % phys_netdev
-        print('%s\t%s\t%s\t%s' % (pci, netdev,
-                                  netdev_get_driver(netdev), suffix))
+            suffix = "VF of {}".format(phys_netdev)
+        print(
+            "{}\t{}\t{}\t{}".format(
+                pci, netdev, netdev_get_driver(netdev), suffix
+            )
+        )
 
 
 def switch():
     """Configure capable devices into switchdev mode"""
-    for pci_addr in os.listdir('/sys/bus/pci/devices'):
+    for pci_addr in os.listdir("/sys/bus/pci/devices"):
         pcidev = PCIDevice(pci_addr)
-        if pcidev.is_pf and pcidev.driver == 'mlx5_core':
-            print('{}: {}'.format(pcidev, pcidev.vfs))
+        if pcidev.is_pf and pcidev.driver == "mlx5_core":
+            print("{}: {}".format(pcidev, pcidev.vfs))
             if pcidev.vfs:
-                if pcidev.devlink_get('eswitch')['mode'] == 'legacy':
+                if pcidev.devlink_get("eswitch")["mode"] == "legacy":
                     rebond = []
                     try:
                         for vf in pcidev.vfs:
                             if vf.bound:
-                                with open('/sys/bus/pci/drivers/'
-                                          'mlx5_core/unbind',
-                                          'wt') as f:
+                                with open(
+                                    "/sys/bus/pci/drivers/" "mlx5_core/unbind",
+                                    "wt",
+                                ) as f:
                                     f.write(vf.pci_addr)
                                 rebond.append(vf)
-                        pcidev.devlink_set('eswitch', 'mode', 'switchdev')
+                        pcidev.devlink_set("eswitch", "mode", "switchdev")
                     finally:
                         for vf in rebond:
-                            with open('/sys/bus/pci/drivers/mlx5_core/bind',
-                                      'wt') as f:
+                            with open(
+                                "/sys/bus/pci/drivers/mlx5_core/bind", "wt"
+                            ) as f:
                                 f.write(vf.pci_addr)
 
 
 def main():
-    parser = argparse.ArgumentParser('mlnx-switchdev-mode')
+    parser = argparse.ArgumentParser("mlnx-switchdev-mode")
     parser.set_defaults(prog=parser.prog)
     subparsers = parser.add_subparsers(
         title="subcommands",
@@ -164,14 +191,13 @@ def main():
         help="sub-command help",
     )
     show_subparser = subparsers.add_parser(
-        'show',
-        help='Show details of installed network adapters'
+        "show", help="Show details of installed network adapters"
     )
     show_subparser.set_defaults(func=show)
 
     switch_subparser = subparsers.add_parser(
-        'switch',
-        help='Switch switchdev capable network adapters to switchdev mode'
+        "switch",
+        help="Switch switchdev capable network adapters to switchdev mode",
     )
     switch_subparser.set_defaults(func=switch)
 
@@ -182,9 +208,4 @@ def main():
     try:
         args.func()
     except Exception as e:
-        raise SystemExit(
-            '{prog}: {msg}'.format(
-                prog=args.prog,
-                msg=e,
-            )
-        )
+        raise SystemExit("{prog}: {msg}".format(prog=args.prog, msg=e))
